d0b933f5b0ddf1a76a76d9d6edc10b3860b0fde6
misso <misso.kang@gmail.com>
2023-01-09 23:16:49 +0900

    회원가입 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require('./home.ctrl');
 router.get("/", ctrl.output.home);
 
 router.get("/login", ctrl.output.login);   // get: url 상에서 받는 경우
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);  // post: body 상에서 받는 경우
 
 
